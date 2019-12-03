ec67a291d566cd7e2007d84f830f4388add71c04
erdinsuharyadi <erdin.sdzeko@gmail.com>
2019-12-03 19:48:55 +0700

    Updates

---
+++ precache-manifest.ee33c00ca7c6011a9458755fb8ccb878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df06d1c3f903398382a8e45b72a55aee",
+    "url": "/erdinsuharyadi/hiringapp-web.git/index.html"
+  },
+  {
+    "revision": "98b1a30053c29c88a837",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fe3bebe30b5cc882d185",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/css/main.23067acd.chunk.css"
+  },
+  {
+    "revision": "98b1a30053c29c88a837",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/js/2.fbab5b0d.chunk.js"
+  },
+  {
+    "revision": "fe3bebe30b5cc882d185",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/js/main.f8454d36.chunk.js"
+  },
+  {
+    "revision": "a580b9c53d6b39ab32b9",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/js/runtime-main.06422e14.js"
+  },
+  {
+    "revision": "9c1222bad2512c42eb2056caef9e5bb1",
+    "url": "/erdinsuharyadi/hiringapp-web.git/static/media/logo-arka.9c1222ba.png"
+  }
+]);
